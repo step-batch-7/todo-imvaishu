10abfdf61b98150e2e58c1de5f96cba472c41e58
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-02-04 13:31:50 +0530

    its rendoring the all titles on page

--- public/script.js
+++ public/script.js
@@ -1,3 +1,28 @@
+let todoList = [];
+
+const updateToHtml = function(todo){
+  return `<h3 id="addNewTodo">${todo.title}</h3>`;
+};
+
+const transferorTitle = function(){
+  const element = document.querySelector('#addNewTodo');
+  todoList = JSON.parse(todoList);
+  const html = todoList.map(updateToHtml).join('');
+  element.innerHTML = html;
+};
+
+const updatePage = function(){
+  const req = new XMLHttpRequest();
+  req.onload = function(){
+    if(this.status == 200){
+      todoList = this.responseText;
+      transferorTitle();
+    }
+  };
+  req.open('GET', 'todoList');
+  req.send();
+};
+
 const showTitle = function(){
   const todoTitle = document.querySelector('#todo-title');
   const titleText = todoTitle.value;
@@ -6,6 +31,12 @@ const showTitle = function(){
   }
   todoTitle.value = '';
   const req = new XMLHttpRequest();
+
+  req.onload = function(){
+    if(this.status == 201){
+      updatePage();
+    }
+  };
 
   req.open('POST', 'todoTitle');
   req.setRequestHeader('Content-Type', 'text/plain');
