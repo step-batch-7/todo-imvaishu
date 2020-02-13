03aaf23adc7f569da4e186181c926bb3a563b73c
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-02-13 17:59:27 +0530

    [vaishnavi|sravani] added methods in the todolist class
    instead of using those from the todo class in the handler
    
    added tests for those methods

--- test/todoListTest.js
+++ test/todoListTest.js
@@ -78,6 +78,70 @@ describe('removeTodo', () => {
   });
 });
 
+describe('addSubTask', () => {
+  it('should add subtask if the todo is there in todoList', () => {
+    const content = '[{"id":"todo_1","title":"something","tasks":[]}]';
+    const todoList = TodoList.load(content);
+    const actual = todoList.addSubTask('todo_1', 'nothing');
+    assert.ok(actual);
+  });
+
+  it('should not add any task if todo is not there', () => {
+    const content = '[{"id":"todo_1","title":"something","tasks":[]}]';
+    const todoList = TodoList.load(content);
+    const actual = todoList.addSubTask('todo_2', 'nothing');
+    assert.ok(!actual);
+  });
+});
+
+describe('deleteTask', () => {
+  it('should delete subtask if the todo is there in todoList', () => {
+    const content = '[{"id":"todo_1","title":"something","tasks":[{"id":"subTask_1","taskTitle":"nothing","status":"false"}]}]';
+    const todoList = TodoList.load(content);
+    const actual = todoList.deleteTask('todo_1', 'subTask_1');
+    assert.ok(actual);
+  });
+
+  it('should not delete any task if todo is not there', () => {
+    const content = '[{"id":"todo_1","title":"something","tasks":[{"id":"subTask_1","taskTitle":"nothing","status":"false"}]}]';
+    const todoList = TodoList.load(content);
+    const actual = todoList.deleteTask('todo_2', 'subTask_2');
+    assert.ok(!actual);
+  });
+});
+
+describe('updateTaskStatus', () => {
+  it('should update status of subtask if it is there in todoList', () => {
+    const content = '[{"id":"todo_1","title":"something","tasks":[{"id":"subTask_1","taskTitle":"nothing","status":"false"}]}]';
+    const todoList = TodoList.load(content);
+    const actual = todoList.updateTaskStatus('todo_1', 'subTask_1', true);
+    assert.ok(actual);
+  });
+
+  it('should not update any status if todo is not there', () => {
+    const content = '[{"id":"todo_1","title":"something","tasks":[{"id":"subTask_1","taskTitle":"nothing","status":"false"}]}]';
+    const todoList = TodoList.load(content);
+    const actual = todoList.updateTaskStatus('todo_2', 'subTask_2', true);
+    assert.ok(!actual);
+  });
+});
+
+describe('editTask', () => {
+  it('should edit subtask  title if todo is there in todoList', () => {
+    const content = '[{"id":"todo_1","title":"something","tasks":[{"id":"subTask_1","taskTitle":"nothing","status":"false"}]}]';
+    const todoList = TodoList.load(content);
+    const actual = todoList.editTask('todo_1', 'subTask_1', 'nothing');
+    assert.ok(actual);
+  });
+
+  it('should not edit subtask title if todo is not there', () => {
+    const content = '[{"id":"todo_1","title":"something","tasks":[{"id":"subTask_1","taskTitle":"nothing","status":"false"}]}]';
+    const todoList = TodoList.load(content);
+    const actual = todoList.editTask('todo_2', 'subTask_2', 'nothing');
+    assert.ok(!actual);
+  });
+});
+
 describe('toJSON', () => {
   it('should convert to json', () => {
     const content = '[{"id":"todo_1","title":"something","tasks":[]}]';
