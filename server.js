538d1249448be48d5c988ccac41487376f374ca5
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-02-14 14:59:53 +0530

    [vaishnavi|sravani] removed redirection in response

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { app } = require('./lib/routers');
 const PORT = 4000;
 
 const main = function(){
-  app.listen(PORT, () => process.stdout.write('listening', PORT));
+  app.listen(PORT, () => process.stdout.write(`listening ${PORT}`));
 };
 
 main();
