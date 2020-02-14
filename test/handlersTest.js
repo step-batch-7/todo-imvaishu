f0d0a40c18c095c3b5c6d64b12a9f0889e1a96b4
Sravani Labala <sravani.labala@gmail.com>
2020-02-14 14:24:29 +0530

    [vaishnavi|sravani] added hasFields function in the handlers
    removed unnecessary if conditions

--- test/handlersTest.js
+++ test/handlersTest.js
@@ -1,86 +1,85 @@
 const request = require('supertest');
 
-const {app} = require('../lib/handlers');
+const { app } = require('../lib/handlers');
 
 describe('POST add todo', () => {
-  it('should add todo to given path', (done) => {
+  it('should add todo to given path', done => {
     request(app)
       .post('/todo')
-      .send({titleText: 'something'})
+      .send({ titleText: 'something' })
       .expect(201, done);
   });
-  it('should not add todo to given path if the fields are not valid', (done) => {
+  it('should not add todo to given path if the fields are not valid', done => {
     request(app)
       .post('/todo')
-      .send({title: 'something'})
-      .expect(404, done);
+      .send({ title: 'something' })
+      .expect(400, done);
   });
 });
 
 describe('POST add sub task', () => {
-  it('should add sub task to given path', (done) => {
+  it('should add sub task to given path', done => {
     request(app)
       .post('/subTask')
-      .send({'todoId': 'todo_1', 'titleText': 'something'})
+      .send({ todoId: 'todo_1', titleText: 'something' })
       .expect(201, done);
   });
-  it('should not add sub task to given path if the fields are not valid', (done) => {
+  it('should not add sub task to given path if the fields are not valid', done => {
     request(app)
       .post('/subTask')
-      .send({'id': 'todo_1', 'titleText': 'something'})
-      .expect(404, done);
+      .send({ id: 'todo_1', titleText: 'something' })
+      .expect(400, done);
   });
 });
 
 describe('POST update subtask status', () => {
-  it('should update status of particular subtask', (done) => {
+  it('should update status of particular subtask', done => {
     request(app)
       .post('/updateStatus')
-      .send({'todoId': 'todo_1', 'subtaskId': 'subTask_0', 'status': true})
+      .send({ todoId: 'todo_1', subtaskId: 'subTask_0', status: true })
       .expect(201, done);
   });
-  it('should not update status of particular subtask if the fields are not valid', (done) => {
+  it('should not update status of particular subtask if the fields are not valid', done => {
     request(app)
       .post('/updateStatus')
-      .send({'id': 'todo_1', 'subtaskId': 'subTask_0', 'sta': true})
-      .expect(404, done);
+      .send({ id: 'todo_1', subtaskId: 'subTask_0', sta: true })
+      .expect(400, done);
   });
 });
 
 describe('POST edit title', () => {
-  it('should edit title of particular todo', (done) => {
+  it('should edit title of particular todo', done => {
     request(app)
       .post('/editTitle')
-      .send({'titleId': 'todo_1', 'titleText': 'Nothing'})
+      .send({ titleId: 'todo_1', titleText: 'Nothing' })
       .expect(201, done);
   });
-  it('should not edit title of particular todo if the fields are not valid', (done) => {
+  it('should not edit title of particular todo if the fields are not valid', done => {
     request(app)
       .post('/editTitle')
-      .send({'id': 'todo_1', 'title': 'Nothing'})
-      .expect(404, done);
+      .send({ id: 'todo_1', title: 'Nothing' })
+      .expect(400, done);
   });
 });
 
 describe('POST edit subtask title', () => {
-  it('should edit title of particular subtask', (done) => {
+  it('should edit title of particular subtask', done => {
     request(app)
       .post('/editSubtask')
-      .send({'todoId': 'todo_1', 'subtaskId': 'subTask_1', 'titleText': 'Nothing'})
+      .send({ todoId: 'todo_1', subtaskId: 'subTask_1', titleText: 'Nothing' })
       .expect(201, done);
   });
-  it('should not edit title of particular subtask if the fields are not valid', (done) => {
+  it('should not edit title of particular subtask if the fields are not valid', done => {
     request(app)
       .post('/editSubtask')
-      .send({'id': 'todo_1', 'subtaskId': 'subTask_1', 'title': 'Nothing'})
-      .expect(404, done);
+      .send({ id: 'todo_1', subtaskId: 'subTask_1', title: 'Nothing' })
+      .expect(400, done);
   });
 });
 
 describe('GET home page', () => {
-  it('should get index.html / path given ', (done) => {
+  it('should get index.html / path given ', done => {
     request(app)
-
       .get('/')
       .set('Accept', '*/*')
       .expect(200)
@@ -90,9 +89,8 @@ describe('GET home page', () => {
 });
 
 describe('GET other page', () => {
-  it('should get other page css path given ', (done) => {
+  it('should get other page css path given ', done => {
     request(app)
-
       .get('/css/style.css')
       .set('Accept', '*/*')
       .expect(200)
@@ -101,9 +99,8 @@ describe('GET other page', () => {
 });
 
 describe('GET Not found', () => {
-  it('should get not found if wrong path given', (done) => {
+  it('should get not found if wrong path given', done => {
     request(app)
-
       .get('/notfound')
       .set('Accept', '*/*')
       .expect(404, done);
@@ -111,9 +108,8 @@ describe('GET Not found', () => {
 });
 
 describe('PUT Method not allowed', () => {
-  it('should return method not allowed', (done) => {
+  it('should return method not allowed', done => {
     request(app)
-
       .put('/notfound')
       .set('Accept', '*/*')
       .expect(404, done);
@@ -121,44 +117,44 @@ describe('PUT Method not allowed', () => {
 });
 
 describe('GET serveTodoList', () => {
-  it('should get todoList /todoList path given ', (done) => {
+  it('should get todoList /todoList path given ', done => {
     request(app)
-
       .get('/todoList')
       .set('Accept', '*/*')
       .expect(200)
-      .expect('[{"id":"todo_1","title":"Nothing","tasks":[{"taskTitle":"Nothing","id":"subTask_1","checked":false}]}]', done);
+      .expect(
+        '[{"id":"todo_1","title":"Nothing","tasks":[{"taskTitle":"Nothing","id":"subTask_1","checked":false}]}]',
+        done
+      );
   });
 });
 
 describe('POST delete subTask', () => {
-  it('should delete particular subtask', (done) => {
+  it('should delete particular subtask', done => {
     request(app)
       .post('/deleteSubtask')
-      .send({'todoId': 'todo_1', 'subtaskId': 'subTask_1'})
+      .send({ todoId: 'todo_1', subtaskId: 'subTask_1' })
       .expect(201, done);
   });
-  it('should delete particular subtask if the fields are not valid', (done) => {
+  it('should delete particular subtask if the fields are not valid', done => {
     request(app)
       .post('/deleteSubtask')
-      .send({'id': 'todo_1', 'subtask': 'subTask_1'})
-      .expect(404, done);
+      .send({ id: 'todo_1', subtask: 'subTask_1' })
+      .expect(400, done);
   });
 });
 
 describe('POST delete todo', () => {
-  it('should delete particular todo', (done) => {
+  it('should delete particular todo', done => {
     request(app)
-
       .post('/deleteTodo')
-      .send({'todoId': 'todo_1'})
+      .send({ todoId: 'todo_1' })
       .expect(201, done);
   });
-  it('should not delete particular todo if the fields are not valid', (done) => {
+  it('should not delete particular todo if the fields are not valid', done => {
     request(app)
-
       .post('/deleteTodo')
-      .send({'id': 'todo_1'})
-      .expect(404, done);
+      .send({ id: 'todo_1' })
+      .expect(400, done);
   });
 });
