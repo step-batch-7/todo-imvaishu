0b97f159955148e9a316add57e769d33da0f1400
Sravani Labala <sravani.labala@gmail.com>
2020-02-17 22:13:17 +0530

    [vaishnavi|sravani] its showing username or password is incorrect
    fixed a bug while adding new user
    changed css

--- public/scripts/signUp.js
+++ public/scripts/signUp.js
@@ -1,19 +1,21 @@
-const validateUserInfo = function(userName, password, confirmPassword){
-  return !userName || !password || !confirmPassword || password !== confirmPassword;
+const validateUserInfo = function(userName, password, confirmPassword) {
+  return (
+    !userName || !password || !confirmPassword || password !== confirmPassword
+  );
 };
 
-const saveAndProceed = function(){
+const saveAndProceed = function() {
   const info = Array.from(document.querySelectorAll('input'));
   const [userName, password, confirmPassword] = info.map(element => {
     const value = element.value;
     element.value = '';
     return value;
   });
-  if(validateUserInfo(userName, password, confirmPassword)){
+  if (validateUserInfo(userName, password, confirmPassword)) {
     return;
   }
-  const content = JSON.stringify({userName, password});
-  xhrPostRequest('newUser', content, (text) => {
+  const content = JSON.stringify({ userName, password });
+  xhrPostRequest('newUser', content, text => {
     const message = document.querySelector('.status');
     message.innerHTML = `<p>${text} : with this Username ${userName}</p>`;
     message.style.display = 'block';
@@ -21,14 +23,28 @@ const saveAndProceed = function(){
   showLogin();
 };
 
-const signUp = function () {
+const signUp = function() {
   document.querySelector('.signUp').style.display = 'flex';
   document.querySelector('#login').style.display = 'none';
-  document.querySelector('.container').style.display = 'none';
 };
 
-const showLogin = function(){
-  document.querySelector('.container').style.display = 'none';
+const showLogin = function() {
   document.querySelector('.signUp').style.display = 'none';
   document.querySelector('#login').style.display = 'flex';
+};
+
+const checkValidUser = function(text) {
+  const { isValid } = JSON.parse(text);
+  if (!isValid) {
+    document.querySelector('h5').classList.remove('hide');
+    return;
+  }
+  document.location = '/homepage.html';
+};
+
+const login = function() {
+  const userName = document.querySelector('.userName').value;
+  const password = document.querySelector('.password').value;
+  const body = JSON.stringify({ userName, password });
+  xhrPostRequest('home', body, checkValidUser);
 };
