efb6384f6ff40aada861f263f259939193e75722
Sravani Labala <sravani.labala@gmail.com>
2020-02-18 10:22:00 +0530

    [vaishnavi|sravani] modified css for the sign Up and login page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,109 +2,110 @@ const selector = function(id) {
   return document.querySelector(id);
 };
 
-const transferorTitle = function(id, html){
+const transferorTitle = function(id, html) {
   const element = selector(id);
   element.innerHTML = html;
 };
 
-const loadTasks = function(){
-  xhrGetRequest('todoList', (responseText) => {
+const loadTasks = function() {
+  xhrGetRequest('todoList', responseText => {
     const todoList = TodoList.load(responseText);
     transferorTitle('#todo-tasks', todoList.tasksToHtml());
   });
 };
 
-const renderTask = function(){
+const renderTask = function() {
   const todoTitle = selector('#todo-title');
   const titleText = todoTitle.value;
-  if(titleText === ''){
-    return alert('Please Enter Title' );
+  if (titleText === '') {
+    return alert('Please Enter Title');
   }
   todoTitle.value = '';
-  const body = JSON.stringify({titleText});
+  const body = JSON.stringify({ titleText });
   xhrPostRequest('todo', body, loadTasks);
 };
 
-const addSubTask = function(id){
+const addSubTask = function(id) {
   const subTaskTitle = selector(`#${id}`);
   const titleText = subTaskTitle.value;
-  if(titleText === ''){
-    return alert('Please Enter subTask..' );
+  if (titleText === '') {
+    return alert('Please Enter subTask..');
   }
   subTaskTitle.value = '';
   const [, element] = id.split('-');
   const todoId = selector(`#${element}`).id;
-  const content = JSON.stringify({todoId, titleText});
+  const content = JSON.stringify({ todoId, titleText });
   xhrPostRequest('subTask', content, loadTasks);
 };
 
-const clearTask = function(id){
-  if(!confirm('Are you sure, you want to delete?')){
+const clearTask = function(id) {
+  if (!confirm('Are you sure, you want to delete?')) {
     return;
   }
   const [, subtaskId, todoId] = id.split('-');
-  const content = JSON.stringify({todoId, subtaskId});
+  const content = JSON.stringify({ todoId, subtaskId });
   xhrPostRequest('deleteSubtask', content, loadTasks);
 };
 
-const clearTodo = function(id){ 
-  if(!confirm('Are you sure, you want to delete?')){
+const clearTodo = function(id) {
+  if (!confirm('Are you sure, you want to delete?')) {
     return;
   }
   const [, todoId] = id.split('-');
   selector('#todo-tasks').innerHTML = '';
-  const body = JSON.stringify({todoId});
+  const body = JSON.stringify({ todoId });
   xhrPostRequest('deleteTodo', body, loadTasks);
 };
 
-const updateStatus = function(id){
-  const[, subtaskId, todoId] = id.split('-');
+const updateStatus = function(id) {
+  const [, subtaskId, todoId] = id.split('-');
   const status = selector(`#${id}`).checked;
-  const content = JSON.stringify({todoId, subtaskId, status});
+  const content = JSON.stringify({ todoId, subtaskId, status });
   xhrPostRequest('updateStatus', content, loadTasks);
 };
 
-const editTodoTitle = function(id){
+const editTodoTitle = function(id) {
   const titleText = selector(`#${id}`).innerText;
   const [, titleId] = id.split('-');
-  const content = JSON.stringify({titleId, titleText});
+  const content = JSON.stringify({ titleId, titleText });
   xhrPostRequest('editTitle', content, loadTasks);
 };
 
-const editSubtask = function(id){
+const editSubtask = function(id) {
   const [, subtaskId, todoId] = id.split('-');
   const titleText = selector(`#${id}`).innerText;
 
-  const content = JSON.stringify({todoId, subtaskId, titleText});
+  const content = JSON.stringify({ todoId, subtaskId, titleText });
   xhrPostRequest('editSubtask', content, loadTasks);
 };
 
-const search = () => selector('#option').value === 'title' ? searchTitle() : searchTask();
+const search = () =>
+  selector('#option').value === 'title' ? searchTitle() : searchTask();
 
-const searchTitle = function(){
+const searchTitle = function() {
   const searchText = selector('#search-title').value;
   const [todoList] = new Array(document.querySelectorAll('.block-container'));
-  todoList.forEach((todo) => {
+  todoList.forEach(todo => {
     todo.style.display = 'none';
   });
-  todoList.forEach((todo) => {
+  todoList.forEach(todo => {
     const title = todo.children[0].innerText;
-    if(title.includes(searchText)){
+    if (title.includes(searchText)) {
       todo.style.display = 'block';
     }
   });
 };
 
-const searchTask = function(){
+const searchTask = function() {
   const searchText = selector('#search-title').value;
   const [todoList] = new Array(document.querySelectorAll('.block-container'));
-  todoList.forEach((todo) => {
+  todoList.forEach(todo => {
     todo.style.display = 'none';
-  }); 
+  });
   const subTasks = new Array(document.querySelectorAll('.subTask'));
-  subTasks[0].forEach((task) => {
+  subTasks[0].forEach(task => {
     const title = task.innerText;
-    if(title.includes(searchText)){
+    if (title.includes(searchText)) {
       task.parentElement.style.display = 'block';
     }
   });
