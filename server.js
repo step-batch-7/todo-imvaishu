b2dd4fd0674247fa64b2cb9e05be75a60d4c0fdf
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-02-20 12:01:55 +0530

    added env variable for port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { app } = require('./lib/routers');
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const main = function(){
   app.listen(PORT, () => process.stdout.write(`listening ${PORT}`));
